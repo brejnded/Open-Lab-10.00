403a1e30a6c6c43a23abf0a00eb47a7e1884d8fe
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-11-16 18:59:05 +0100

    Update Program.cs

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Apps Lab!");
         }
     }
 }
